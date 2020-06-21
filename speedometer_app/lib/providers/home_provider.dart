24e6c4b77135931eb68c6fdf41e17587e0a486dd
EmaanAhmed <58292190+EmaanAhmed@users.noreply.github.com>
2020-06-21 21:10:54 +0200

    time calculated using geolocator

--- speedometer_app/lib/providers/home_provider.dart
+++ speedometer_app/lib/providers/home_provider.dart
@@ -1,25 +1,24 @@
-import 'dart:async';
-import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:sensors/sensors.dart';
 
 class HomeProvider with ChangeNotifier {
-  var _currentSpeed;
-  var _currentAcceleration;
-  var _takenTimeFromTen;
-  var _takenTimeToTen;
-  List<StreamSubscription<dynamic>> _streamSubscriptions;
+  int _currentSpeed;
+  // var _currentAcceleration;
+  int _takenTimeFromTen;
+  int _takenTimeToTen;
+  DateTime _isTen;
+  DateTime _isthirty;
+  DateTime _isTenAgain;
+  // List<StreamSubscription<dynamic>> _streamSubscriptions;
 
   HomeProvider() {
     _currentSpeed = 0;
-    _currentAcceleration = 0;
     _takenTimeFromTen = 0;
     _takenTimeToTen = 0;
-    _streamSubscriptions = <StreamSubscription<dynamic>>[];
+
+    // _currentAcceleration = 0;
+    // _streamSubscriptions = <StreamSubscription<dynamic>>[];
     calcCurrentSpeed();
-    calcTimeFromTen();
-    calcTimeToTen();
   }
 
   // Getters
@@ -48,36 +47,64 @@ class HomeProvider with ChangeNotifier {
     var options = LocationOptions(accuracy: LocationAccuracy.high);
     geolocator.getPositionStream(options).listen((position) {
       setCurrentSpeed((position.speed * 3.6).toInt());
+      if(_isTen == null && _isthirty == null && _currentSpeed >= 10 && _currentSpeed < 30 ){
+        _isTen = DateTime.now();
+      }
+      if(_isthirty == null && _isTen != null && _currentSpeed >= 30) {
+        _isthirty = DateTime.now();
+      }
+      if(_isTen != null && _isthirty != null) {
+        calcTimeFromTen();
+      }
+      if(_isthirty!= null && _isTenAgain == null && _currentSpeed >= 10 && _currentSpeed < 30){
+        _isTenAgain = DateTime.now();
+      }
+      if(_isTenAgain != null && _isthirty != null) {
+        calcTimeToTen();
+      }
+      if(_isTen!= null && _isTenAgain!= null && _isthirty!=null) {
+        _isTen = null;
+        _isTenAgain = null;
+        _isthirty = null;
+      }
     });
   }
 
-  void calcTimeFromTen() {
-    var initialVelocity = 10 / (1000 / 3600); //km/h -> m/s
-    var finalVelocity = 30 / (1000 / 3600); //km/h -> m/s
-
-    var _deltaVelocity = finalVelocity - initialVelocity;
-    _streamSubscriptions
-        .add(accelerometerEvents.listen((AccelerometerEvent event) {
-      var x = event.x;
-      var y = event.y;
-      var z = event.z;
-      _currentAcceleration = (sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2))).abs();
-      setTakenTimeFromTen((_deltaVelocity ~/ _currentAcceleration).abs());
-    }));
+  calcTimeFromTen(){
+    setTakenTimeFromTen(_isthirty.difference(_isTen).inSeconds);
   }
 
-  void calcTimeToTen() {
-    var initialVelocity = 30 / (1000 / 3600); //km/h -> m/s
-    var finalVelocity = 10 / (1000 / 3600); //km/h -> m/s
-
-    var _deltaVelocity = finalVelocity - initialVelocity;
-    _streamSubscriptions
-        .add(accelerometerEvents.listen((AccelerometerEvent event) {
-      var x = event.x;
-      var y = event.y;
-      var z = event.z;
-      _currentAcceleration = (sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2))).abs();
-      setTakenTimeToTen((_deltaVelocity ~/ _currentAcceleration).abs());
-    }));
+  calcTimeToTen(){
+    setTakenTimeToTen(_isTenAgain.difference(_isthirty).inSeconds);
   }
+
+  // void calcTimeFromTen() {
+  //   var initialVelocity = 10 / (1000 / 3600); //km/h -> m/s
+  //   var finalVelocity = 30 / (1000 / 3600); //km/h -> m/s
+
+  //   var _deltaVelocity = finalVelocity - initialVelocity;
+  //   _streamSubscriptions
+  //       .add(accelerometerEvents.listen((AccelerometerEvent event) {
+  //     var x = event.x;
+  //     var y = event.y;
+  //     var z = event.z;
+  //     _currentAcceleration = (sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2))).abs();
+  //     setTakenTimeFromTen((_deltaVelocity ~/ _currentAcceleration).abs());
+  //   }));
+  // }
+
+  // void calcTimeToTen() {
+  //   var initialVelocity = 30 / (1000 / 3600); //km/h -> m/s
+  //   var finalVelocity = 10 / (1000 / 3600); //km/h -> m/s
+
+  //   var _deltaVelocity = finalVelocity - initialVelocity;
+  //   _streamSubscriptions
+  //       .add(accelerometerEvents.listen((AccelerometerEvent event) {
+  //     var x = event.x;
+  //     var y = event.y;
+  //     var z = event.z;
+  //     _currentAcceleration = (sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2))).abs();
+  //     setTakenTimeToTen((_deltaVelocity ~/ _currentAcceleration).abs());
+  //   }));
+  // }
 }
